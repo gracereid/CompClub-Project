3d0360cd6c722f2ae1e921871b51d326bd411a6e
Grace Reid <grace.e.reid>
2017-03-05 22:45:36 -0500

    Literally like I'm not even doing anything meaningful yet so don't judge the appearance

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
-<html>
+
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Computer Club</title>
+
+ <link href="index.css" rel="stylesheet">
+
 </head>
+
 <body>
-	
-</body>	
-	
+	<header>
+		<table>
+			<tr>
+				<th colspan="3"><h1>WMU Computer Club</h1></th>
+			</tr>
+			<tr>
+				<td><a href="">Make A Purchase</a></td>
+				<td><a href="">About the Club</a></td>
+				<td></td>
+			</tr>
+		</table>
+		<form id="logIn">
+			<caption><h2>Log In:</h2></caption>
+			 <p>Username: <input type="text" name="username" value="" id="username"/></p>
+			 <p>Password: <input type="text" name="password" value="" id="password"/></p>
+			 <input type="submit" name="submit">
+		</form>
+		<h3>Don't have an account? <a href="">Register Here</a></h3>
+	</header>
+
+
+  
+</body>
 </html>
