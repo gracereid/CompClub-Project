11194dcaf2b07fb9d8be6ac3992e75b8a298ea0e
Michelle Vittini <kmd1156@wmich.edu>
2017-04-13 09:54:04 -0400

    Admin interface
    Added more pages to the admin interface. No fucntionality added yet.

--- databases/load.sql
+++ databases/load.sql
@@ -21,18 +21,43 @@ CREATE DATABASE Cclub_shop;
 USE Cclub_shop;
 
 CREATE TABLE customers(
-	ID	VARCHAR(100)		NOT NULL,
-	name	VARCHAR(20)		NOT NULL,
+	ID		VARCHAR(100)		NOT NULL,
+	name	VARCHAR(20)			NOT NULL,
 	balance	DECIMAL(3,2)		NOT NULL,
 	type 	CHAR(1),
 	PRIMARY KEY (ID)
 );
 
 CREATE TABLE products(
-	ID			INT		NOT NULL,
+	ID				INT				NOT NULL,
+	name 			VARCHAR(20)		NOT NULL,
+	type 			VARCHAR(20)		NOT NULL,
 	price 			DECIMAL(3,2) 	NOT NULL,
 	org_price 		DECIMAL(3,2) 	NOT NULL,
-	quantity		INT 		NOT NULL,
-	type 			VARCHAR(20)	NOT NULL,
+	quantity		INT 			NOT NULL,
+
 	PRIMARY KEY(ID)
+);
+
+CREATE TABLE admin(
+	ID				INT				NOT NULL,
+	nick	 		VARCHAR(20)		NOT NULL,
+	position 		VARCHAR(20)		NOT NULL,
+	phone			VARCHAR(20)		NOT NULL,
+
+	PRIMARY KEY(ID)
+);
+
+CREATE TABLE sold(
+	ID				INT				NOT NULL,
+	name			VARCHAR(20)		NOT NULL,
+	type 			VARCHAR(20)		NOT NULL,
+	price 			DECIMAL(3,2) 	NOT NULL,
+	quantity		INT 			NOT NULL,
+	when			DATE()			NOT NULL,
+
+	PRIMARY KEY(ID),
+	FOREIGN KEY (ID) REFERENCES products(ID),
+	FOREIGN KEY (price) REFERENCES products(price),
+	FOREIGN KEY (type) REFERENCES products(type)
 );
