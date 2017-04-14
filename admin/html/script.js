578aed67a86ed6967238ac11cc8474722595dc14
Michelle Vittini <kmd1156@wmich.edu>
2017-04-14 12:29:00 -0400

    Added JS for more Dynamic effect

---
+++ admin/html/script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+	document.getElementById('table1').style.display = 'none';
+	document.getElementById('table2').style.display = 'none';
+	document.getElementById('table3').style.display = 'none';
+}
+
+function show(nr) {
+	document.getElementById("table1").style.display="none";
+    document.getElementById("table2").style.display="none";
+    document.getElementById("table3").style.display="none";
+    document.getElementById("table"+nr).style.display="block";
+ 
+}
