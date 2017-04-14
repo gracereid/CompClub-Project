ab5e47c504ee71db94678bfab549e8bc90016fcf
Michelle Vittini <kmd1156@wmich.edu>
2017-04-14 14:09:18 -0400

    Added more js to the rest of the pages
    Will be adding php soon for complete functionality

---
+++ admin/html/add_admin.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+	document.getElementById('t1').style.display = 'none';
+	document.getElementById('t2').style.display = 'none';
+}
+
+function show(nr) {
+	document.getElementById("t1").style.display="none";
+	document.getElementById("t2").style.display="none";
+    document.getElementById("t"+nr).style.display="block";
+ 
+}
