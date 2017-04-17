a1d302210040fa00b3025e97221b8ddc6bbb1a2d
Grace Reid <grace.e.reid>
2017-04-17 01:01:03 -0400

    Fixed the sql in the pay php so it updates the inventory to reflect the sale.

--- home/pay.php
+++ home/pay.php
@@ -1,13 +1,34 @@
 <?php
 session_start();
 
-$_SESSION['balance']+=$_SESSION['tempTotal'];
 
 for($x=0; $x<count($_SESSION['ShoppingCart']); $x++)
 	{
   		$_SESSION['orderHistory'][][0]=$_SESSION['ShoppingCart'][$x][1];//name
   		$_SESSION['orderHistory'][][1]=$_SESSION['ShoppingCart'][$x][2];//name
+  		
+  		//update inventory
+		try{
+				$connString = "mysql:host=localhost; dbname=Cclub_shop";
+				$user = "cclub";
+				$pass = "cclub"; 
+				$pdo = new PDO($connString, $user, $pass);
+				$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+				$sql='UPDATE products set quantity=quantity-1 where name="'.$_SESSION['ShoppingCart'][$x][1].'"';
+						$result = $pdo->query($sql);
+				echo '</br>'.$sql.'<br>';
+				
+				
+				$pdo=null;
+			}
+		catch(PDOException $e)
+		{
+			die ($e -> getMessage());
+		}
 	}
+
+
 
 unset($_SESSION['tempTotal']);
 unset($_SESSION['ShoppingCart']);
